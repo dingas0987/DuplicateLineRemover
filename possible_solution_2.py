db2ac01d07f1eace2b441a2ef4bdad1db4ce3b64
dingas0987 <vincentliang424@gmail.com>
2021-07-10 13:17:26 -0400

    wip 1

--- possible_solution_2.py
+++ possible_solution_2.py
@@ -1,38 +1,55 @@
 from itertools import islice
 
-location = "test_json1.json"
+#location = "test_json1.json"
+location = "test.txt"
 
 # Current Status
 # It deletes the lines
 # Sometimes it doesn't
 # The counter is not very accurate
 
+#                #islice is used to start the iterator in a different spot
+#                start_inner_loop = islice(inner_file, counter, None)
+#                for line_num, dupe_line in enumerate(start_inner_loop, counter):
+#                    if dupe_line == line:
+#                        dupe_count +=1
+#                        print("there is a dupe spotted")
+#                        print("original line #:", counter)
+#                        print("original line:", line)
+#                        print("dupe line #",line_num)
+#                        print("dupe line", dupe_line)
+#                        line_num -=1
+#                        # screw it using the stack overflow solution
+#                        delete_line(directory, line_num)  
+#                        print("removed") 
+#                        print("current duplicates found:", dupe_count)
+#                        continue
 
 def search_dupe(directory):
-    counter = 0
     dupe_count = 0 
+    inner_file_line = 0
+    current_char_pos = 0
     with open(directory, "r+", encoding='utf-8', errors='ignore') as read_file:
         for line in read_file:
-            current_char_pos = len(line) + 1
-            counter += 1
+            current_char_pos = current_char_pos + len(line) + 1
+            print("current char position:", current_char_pos)
+            print("current line:", line)
             with open(directory, "r+", encoding='utf-8', errors='ignore') as inner_file:
-                #islice is used to start the iterator in a different spot
-                start_inner_loop = islice(inner_file, counter, None)
-                for line_num, dupe_line in enumerate(start_inner_loop, counter):
-                    if dupe_line == line:
+                dupe_line = inner_file.readline()
+                while dupe_line:
+                    inner_file_pos = inner_file.tell()
+                    print("current line in dupe file:", dupe_line)
+                    print("inner file position:",inner_file_pos)
+                    if (line == dupe_line) and (current_char_pos != inner_file_pos):
                         dupe_count +=1
                         print("there is a dupe spotted")
-                        print("original line #:", counter)
                         print("original line:", line)
-                        print("dupe line #",line_num)
                         print("dupe line", dupe_line)
-                        line_num -=1
-                        # screw it using the stack overflow solution
-                        delete_line(directory, line_num)  
-                        print("removed") 
-                        print("current duplicates found:", dupe_count)
-                        continue
-                inner_file.close()
+                        #delete_line(directory, inner_file_line)
+                    #inner_file_line+=1 
+                    dupe_line = inner_file.readline()
+                    inner_file_pos = inner_file.tell()
+                    
         print("final dupe count:", dupe_count)
         if dupe_count == 0:
             print("there are no more dupes")
