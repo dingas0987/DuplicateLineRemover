127c03dfdeea754ed36d55c2694af2322ba88942
dingas0987 <vincentliang424@gmail.com>
2021-07-13 10:04:59 -0400

    added function to add and remove blank line at bottom

--- unique_text_writer.py
+++ unique_text_writer.py
@@ -1,12 +1,17 @@
 location = 'test_json1.json'
 result = 'tested_json1_result_1.json'
+#location = "comment_code_data1.json"
+#result = "comment_code_data1_result_2.json"
 
 # Write unique lines to a file
-# If two lines of text have different casing but is the same string
-# it is not included into the resulting file
 
 import os.path
 from os import path
+
+def remove_bottom_blank(file):
+    with open(file, 'rb+') as file_out:
+        file_out.seek(-2, os.SEEK_END)
+        file_out.truncate()
 
 def writeToFile(file, text):
     with open(file, 'a', encoding='utf-8') as newfile:
@@ -16,15 +21,16 @@ def writeToFile(file, text):
 def dupe_search(dir, res):
     dupe_counter = 0
     with open(dir, 'r', encoding='utf-8') as readfile:
-            for line in readfile:
-                if not path.exists(res):
-                        with open(res, 'w', encoding='utf-8') as createfile:
-                            createfile.close()
-                if line.lower() in open(res, encoding='utf-8').read().lower():
-                    dupe_counter += 1
-                else:
-                    writeToFile(res, line)
+        for line in readfile:
+            if not path.exists(res):
+                    with open(res, 'w', encoding='utf-8') as createfile:
+                        createfile.close()
+            if line.lower() in open(res, encoding='utf-8').read().lower():
+                dupe_counter += 1
+            else:
+                writeToFile(res, line)
     print("# of dupes:", dupe_counter)
+    remove_bottom_blank(dir)
     readfile.close()
 
 dupe_search(location, result)
