91a41c4dee1e62e2faa827848d9f8b0a03e0c2ee
dingas0987 <vincentliang424@gmail.com>
2021-07-14 15:41:11 -0400

    combine text file function

--- combine_files.py
+++ combine_files.py
@@ -1,22 +1,27 @@
 import os
 from os import path
 
-def combine_files(file_list, res):
-    # create result file if it does not exist
+def combine_files(filelist, res):
     if not path.exists(res):
         with open(res, 'w', encoding='utf-8') as createfile: 
             createfile.close()
-    for fname in file_list:
-        # files to be copied over to result file
-        with open(fname, 'r+', encoding='utf-8') as input_file:
-            if(input_file.read()[-1] != '\n'):
-                input_file.write("\n")
-            # result file
-            with open(res, 'a', encoding='utf-8') as output_file:
-                for line in input_file:
-                    output_file.write(line)
+    with open(res, 'w') as outfile:
+        for fname in filelist:
+            with open(fname, 'r+', encoding='utf-8') as input_file:
+                if(input_file.read()[-1] != '\n'):
+                    input_file.write("\n")
+                    input_file.close()
+            with open(fname) as infile:
+                for line in infile:
+                    outfile.write(line)
+    outfile.close()
 
-#if __name__ == '__main__':
-#    files = ["text1.txt", "text2.txt", "text3.txt"]
-#    result = "text_result.txt"
-#    combine_files(files, result)
+def writeToFile(file, text):
+    with open(file, 'a', encoding='utf-8') as newfile:
+        newfile.write(text)
+        newfile.close()
+
+if __name__ == '__main__':
+    files = ["text1.txt", "text2.txt", "text3.txt"]
+    result = "text_result.txt"
+    combine_files(files, result)
